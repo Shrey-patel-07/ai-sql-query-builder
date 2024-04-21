bb3dd324748a7b9604c6eda76e2aa53641bf1554
Shrey-patel-07 <shrey07patel@gmail.com>
2024-04-21 13:00:28 +0530

    llama model added, ddl_query_generator

--- ddl_query_generator.py
+++ ddl_query_generator.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
-
+import os
+import sqlite3
 import warnings
+import pandas as pd
+from flask import Flask, render_template, request
 from langchain_community.llms import Ollama
 from langchain import PromptTemplate, LLMChain
 
@@ -11,6 +13,9 @@ warnings.filterwarnings("ignore")
 
 # Initializing the language model
 llm = Ollama(model="pxlksr/defog_sqlcoder-7b-2:Q4_K")
+
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Prompt template for language model
 template = """
@@ -37,10 +42,23 @@ def get_llm_response(user_question, table_metadata_string):
     response = llm_chain.run({"user_question": user_question, "table_metadata_string": table_metadata_string})
     return response
 
+def run_sql_query(csv_files, sql_query):
+    conn = sqlite3.connect(':memory:')
+    
+    for file_path in csv_files:
+        df_name = os.path.splitext(os.path.basename(file_path))[0]  # Extract file name without extension
+        df = pd.read_csv(file_path)
+        df.to_sql(df_name, conn, index=False)
+    
+    result = pd.read_sql_query(sql_query, con=conn)
+    
+    return result.to_html()
+
+
 history = []
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def ddl_query():
     ddl = None
     if request.method == 'POST':
         ddl = request.form['ddl']
@@ -53,5 +71,30 @@ def index():
 
     return render_template('index.html', history=history, ddl=ddl)
 
+
+@app.route('/dbms_query', methods=['GET', 'POST'])
+def index():
+    result = None
+    query = None
+    
+    if request.method == 'POST':
+        uploaded_files = request.files.getlist('file')
+        csv_files = []
+        for file in uploaded_files:
+            if file.filename != '':
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+                file.save(file_path)
+                csv_files.append(file_path)
+        
+        sql_query = request.form['sql_query']
+        query = sql_query
+        
+        # Execute SQL query and store result in history
+        result = run_sql_query(csv_files, sql_query)
+        history.append({'query': sql_query, 'result': result})
+        
+    return render_template('database_selection_index.html', result=result, query=query, history=history)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port = 7860, host = '0.0.0.0')
+
